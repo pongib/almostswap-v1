81d20a789c5547c1664f680d59b9044d1e1a7d29
pongtsu <alwayshungrytolearn@gmail.com>
2022-09-16 19:30:51 +0700

    feat: Swap.

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -6,6 +6,8 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 error Exchange__NotAllowAddressZero();
 error Exchange__InputOrOutputReserveBelowZero();
 error Exchange__BelowZeroAmount(uint256 amount);
+error Exchange__ExceedSlipageTolerance();
+error Exchange__TransferETHFail();
 
 contract Exchange {
     address public s_tokenAddress;
@@ -22,6 +24,51 @@ contract Exchange {
     function addLiquidity(uint256 amount) public payable {
         IERC20 token = IERC20(s_tokenAddress);
         token.transferFrom(msg.sender, address(this), amount);
+    }
+
+    function ethToTokenSwap(uint256 minAmountToReceive) external payable {
+        uint256 ethSoldAmount = msg.value;
+        uint256 tokenReserve = getReserve();
+        uint256 tokenBoughtAmount = getAmount(
+            ethSoldAmount,
+            // when call this function it included msg.value to balance.
+            address(this).balance - msg.value,
+            tokenReserve
+        );
+
+        //protect user from frontrun manipulate price.
+        if (minAmountToReceive > tokenBoughtAmount) {
+            revert Exchange__ExceedSlipageTolerance();
+        }
+        IERC20 token = IERC20(s_tokenAddress);
+        token.transfer(msg.sender, tokenBoughtAmount);
+    }
+
+    function tokenToEthSwap(uint256 tokenSoldAmount, uint256 minAmountToReceive)
+        external
+        payable
+    {
+        uint256 tokenReserve = getReserve();
+        uint256 ethBoughtAmount = getAmount(
+            tokenSoldAmount,
+            tokenReserve,
+            address(this).balance
+        );
+
+        //protect user from frontrun manipulate price.
+        if (minAmountToReceive > ethBoughtAmount) {
+            revert Exchange__ExceedSlipageTolerance();
+        }
+
+        IERC20(s_tokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            tokenSoldAmount
+        );
+        (bool success, ) = address(this).call{value: ethBoughtAmount}("");
+        if (!success) {
+            revert Exchange__TransferETHFail();
+        }
     }
 
     function getReserve() public view returns (uint256) {

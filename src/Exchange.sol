c4b39195b610fae99f2ba9d90748de722a24a4b0
pongtsu <alwayshungrytolearn@gmail.com>
2022-09-16 19:00:14 +0700

    feat: Get token and eth amount.

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 error Exchange__NotAllowAddressZero();
+error Exchange__InputOrOutputReserveBelowZero();
+error Exchange__BelowZeroAmount(uint256 amount);
 
 contract Exchange {
     address public s_tokenAddress;
@@ -24,5 +26,43 @@ contract Exchange {
 
     function getReserve() public view returns (uint256) {
         return IERC20(s_tokenAddress).balanceOf(address(this));
+    }
+
+    function getAmount(
+        uint256 inputAmount,
+        uint256 inputReserve,
+        uint256 outputReserve
+    ) private pure returns (uint256) {
+        if (inputReserve <= 0 || outputReserve <= 0) {
+            revert Exchange__InputOrOutputReserveBelowZero();
+        }
+
+        return (outputReserve * inputAmount) / (inputReserve + inputAmount);
+    }
+
+    function getEthAmount(uint256 tokenSoldAmount)
+        public
+        view
+        returns (uint256)
+    {
+        if (tokenSoldAmount <= 0) {
+            revert Exchange__BelowZeroAmount(tokenSoldAmount);
+        }
+        uint256 ethReserve = address(this).balance;
+        uint256 tokenReserve = getReserve();
+        return getAmount(tokenSoldAmount, tokenReserve, ethReserve);
+    }
+
+    function getTokenAmount(uint256 ethSoldAmount)
+        public
+        view
+        returns (uint256)
+    {
+        if (ethSoldAmount <= 0) {
+            revert Exchange__BelowZeroAmount(ethSoldAmount);
+        }
+        uint256 ethReserve = address(this).balance;
+        uint256 tokenReserve = getReserve();
+        return getAmount(ethSoldAmount, ethReserve, tokenReserve);
     }
 }
--- test/Exchange.t.sol
+++ test/Exchange.t.sol
@@ -11,7 +11,7 @@ contract ExchangeTest is Test {
     Exchange public exchange;
 
     function setUp() public {
-        uint256 amount = 100 ether;
+        uint256 amount = 1000000 ether;
         token = new Token("PONG", "PNG", amount);
         exchange = new Exchange(address(token));
     }
@@ -21,5 +21,26 @@ contract ExchangeTest is Test {
         exchange.addLiquidity{value: 1 ether}(10 ether);
         assertEq(exchange.getReserve(), 10 ether);
         assertEq(address(exchange).balance, 1 ether);
+    }
+
+    function testGetEthAmount() public {
+        token.approve(address(exchange), 2000 ether);
+        exchange.addLiquidity{value: 1000 ether}(2000 ether);
+        uint256 tokenSoldAmount = 2 ether;
+        assertEq(exchange.getEthAmount(tokenSoldAmount), 999000999000999000);
+        // increase more to see more price impact
+
+        assertEq(exchange.getEthAmount(2 ether), 999000999000999000);
+        assertEq(exchange.getEthAmount(100 ether), 47619047619047619047);
+        assertEq(exchange.getEthAmount(2000 ether), 500000000000000000000);
+    }
+
+    function testGetTokenAmount() public {
+        token.approve(address(exchange), 2000 ether);
+        exchange.addLiquidity{value: 1000 ether}(2000 ether);
+        uint256 ethSoldAmount = 1 ether;
+        assertEq(exchange.getTokenAmount(ethSoldAmount), 1998001998001998001);
+        assertEq(exchange.getTokenAmount(100 ether), 181818181818181818181);
+        assertEq(exchange.getTokenAmount(1000 ether), 1000000000000000000000);
     }
 }
